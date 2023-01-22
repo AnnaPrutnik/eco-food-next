e64ba6c32b5f0492ccf04837bfa315baf20a2ce7
Anna Prutnik <prutnik.anna@gmail.com>
2023-01-22 19:54:59 +0200

    feature: add input-slider

--- src/components/filter/form.tsx
+++ src/components/filter/form.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Formik, Field } from 'formik';
-import { Box, CheckboxGroup, Heading, Input, Select } from 'components';
+import { Box, CheckboxGroup, Heading, Input, Select, Slider } from 'components';
 import { brands as defaultBrands, country, filterProducts } from './db';
 import { ICheckBox } from 'types';
 import { StyledForm } from './form.styled';
@@ -31,7 +31,7 @@ export const FormTest: React.FC<FormTestProps> = () => {
           filtering: filterProducts[0].value,
           brands: [],
           priceFrom: 0,
-          priceTo: 20,
+          priceTo: 150,
         }}
         onSubmit={(values) => {
           console.log(values);
@@ -40,15 +40,16 @@ export const FormTest: React.FC<FormTestProps> = () => {
         <StyledForm>
           <Input type='checkout' name='firstName' label='First name' />
           <Input type='email' name='sendEmail' />
-          <Box
-            display='flex'
-            justifyContent='space-between'
-            alignItems='center'
-          >
-            <p>From: </p>
-            <Input type='price' name='priceFrom' label='$' />
-            <p>To</p>
-            <Input type='price' name='priceTo' label='$' />
+          <Slider start='priceFrom' end='priceTo' max={500} />
+          <Box display='flex' alignItems='center'>
+            <Box mr={5} display='flex' alignItems='center'>
+              <p style={{ opacity: 0.5, marginRight: '12px' }}>From: </p>
+              <Input type='price' name='priceFrom' label='$' />
+            </Box>
+            <Box display='flex' alignItems='center'>
+              <p style={{ opacity: 0.5, marginRight: '12px' }}>To</p>
+              <Input type='price' name='priceTo' label='$' />
+            </Box>
           </Box>
 
           <Select
