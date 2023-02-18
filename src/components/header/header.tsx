19cede443c02d41eef591d9c1a8728a0fdcdcb28
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2023-02-18 11:01:21 +0200

    Добавил компонент categoriesMenu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
+import { Box, SearchForm, CategoriesMenu } from "components";
+import { Logo } from "components/svg";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Box
+      as="header"
+      height={{ _: "64px", lg: "88px" }}
+      px={{ _: "16px", lg: "48px" }}
+      display="flex"
+      alignItems="center"
+    >
+      <Box display="flex" alignItems="center" gridGap="40px">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <SearchForm />
+        <CategoriesMenu />
+      </Box>
+    </Box>
+  );
 };
