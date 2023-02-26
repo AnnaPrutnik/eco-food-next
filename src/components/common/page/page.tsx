067db4e5004f835fb7f5ea5134fc825e7fe52ae1
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2023-02-26 23:24:16 +0200

    Добавил точку перелома 1024px

--- src/components/common/page/page.tsx
+++ src/components/common/page/page.tsx
@@ -8,7 +8,7 @@ interface PageProps {
 export const Page: React.FC<PageProps> = ({ children }) => {
   return (
     <Box
-      width={['100%', '375px', '768px', '1200px', '1440px']}
+      width={['100%', '375px', '768px', '1024px', '1200px', '1440px']}
       padding={['sp16', 'sp24', 'sp40', 'sp64', `64px 135px`]}
       margin="0 auto"
     >
