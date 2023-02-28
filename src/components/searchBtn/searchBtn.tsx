d54436e834f7bedeb3b7b6679ce01c0f33238b1d
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2023-02-28 23:39:46 +0200

    Добавил функционал для кнопки поиска в мобильной версии

--- src/components/searchBtn/searchBtn.tsx
+++ src/components/searchBtn/searchBtn.tsx
@@ -1,18 +1,30 @@
+import { SearchForm } from "components";
+import { Root, Trigger, Portal, Content } from "./searchBtn.styled";
 import { SearchIcon } from "components/svg";
 import { Box } from "components";
 import { theme } from "theme";
 
 export const SearchBtn = () => {
   return (
-    <Box
-      as="button"
-      display={["flex", "flex", "none", "none"]}
-      p="7px 0"
-      border="none"
-      color={theme.colors.text}
-      backgroundColor="transparent"
-    >
-      <SearchIcon size={24} />
-    </Box>
+    <Root>
+      <Trigger>
+        <SearchIcon size={24} />
+      </Trigger>
+      <Portal>
+        <Content>
+          <SearchForm />
+        </Content>
+      </Portal>
+    </Root>
+    // <Box
+    //   as="button"
+    //   display={["flex", "flex", "none", "none"]}
+    //   p="7px 0"
+    //   border="none"
+    //   color={theme.colors.text}
+    //   backgroundColor="transparent"
+    // >
+    //   <SearchIcon size={24} />
+    // </Box>
   );
 };
