b8f677e1cf4b82c2cfaad3679ea8490c37458415
Anna Prutnik <prutnik.anna@gmail.com>
2023-03-13 14:11:34 +0200

    fix: remove select padding, add overflow

--- src/screen/product/product.tsx
+++ src/screen/product/product.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Page, Box, Breadcrumbs, CategoryTitle } from 'components';
 
 export const ProductScreen = () => {
-	return <div>ProductScreen</div>;
+	return (
+		<Page>
+			<Box display='flex' flexDirection='column' gridGap='sp16'>
+				<Breadcrumbs />
+				<CategoryTitle text='Product Details' />
+			</Box>
+		</Page>
+	);
 };
